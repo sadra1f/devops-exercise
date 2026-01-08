2c822859233d9072de420c839755b2f2da9a6667
Sadra Saderi <sadrassx@gmail.com>
2026-01-08 12:09:15 +0330

    Add dummy accessible server script

---
+++ bash/004-accessible-server.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Write a program that has the IP of a server and its User/Pass in front of
+# the Script name and if it is pingable, sends its /etc/passwd file
+# to /home/user path of that server, otherwise a message displayed that
+# the server is not accessible.
+
+echo "Not implemented"
