d9a58129da54564ac9b7c4e30478974aa2b466ab
Sadra Saderi <sadrassx@gmail.com>
2026-01-08 11:45:12 +0330

    Add min max script

---
+++ bash/003-min-max.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+min_number=0
+max_number=0
+
+for index in {1..20}; do
+    while [[ true ]]; do
+        read -p "($(printf %02d $index)/20) Enter a number: " number
+
+        if ! [[ $number =~ ^[0-9]+$ ]]; then
+            echo "ERROR: Number is not valid!"
+            continue
+        fi
+
+        if [[ $number -gt $max_number ]]; then
+            max_number=$number
+        fi
+        if [[ $number -lt $min_number ]]; then
+            min_number=$number
+        fi
+
+        break
+    done
+done
+
+echo "MAX: $max_number"
+echo "MIN: $min_number"
